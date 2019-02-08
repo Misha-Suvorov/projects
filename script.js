8926a6aba2c5f12989e60b8584f3fee2739b7b20
Misha-Suvorov <47455489+Misha-Suvorov@users.noreply.github.com>
2019-02-08 17:50:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var number = -2;
+var number1 = 5;
+var number2 = 2;
+var temp = 0;
+if (number < number1){
+	temp = number;
+}
+else{
+	temp = number1;
+}
+if (temp < number2 ){
+	temp = temp;
+}
+else{
+	temp = number2;
+}
+console.log (temp);
